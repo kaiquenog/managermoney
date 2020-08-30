21d45ee08c9407b44e86187920448b1888ef109d
kaique <kaiquenogueir@gmail.com>
2020-08-30 00:01:15 -0300

    Adicionando FlyWayDb e o diretorio db\migrate , criado o docker-compose para criaçao do container com o banco de dados

---
+++ src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
@@ -0,0 +1,7 @@
+CREATE TABLE categoria (
+codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+nome   VARCHAR(50) not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert into categoria (nome) values ('LAZER');
+insert into categoria (nome) values ('FAMARCIA');
