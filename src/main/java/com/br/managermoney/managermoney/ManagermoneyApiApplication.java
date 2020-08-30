b9c9118a484e714a35b5731c71f4c88bafa5a579
kaique <kaiquenogueir@gmail.com>
2020-08-29 22:46:46 -0300

    Inicio do Projeto Angular com Spring

---
+++ src/main/java/com/br/managermoney/managermoney/ManagermoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.managermoney.managermoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagermoneyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagermoneyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/managermoney/managermoney/ManagermoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.managermoney.managermoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagermoneyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
